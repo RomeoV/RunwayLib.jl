2c93ae970102e43acde774a6fc5a5ccc16b2bc92
Romeo Valentin <romeov@stanford.edu>
2025-08-15 14:41:20 +0200

    Make easier precompile problem
    to avoid maxiters

--- src/precompile_workloads.jl
+++ src/precompile_workloads.jl
@@ -20,8 +20,8 @@ PrecompileTools.@compile_workload begin
     ]
 
     # Define typical aircraft position and orientation
-    aircraft_pos = WorldPoint(-1300.0m, 0.0m, 80.0m)
-    aircraft_rot = RotZYX(roll=0.03, pitch=0.04, yaw=0.05)
+    aircraft_pos = WorldPoint(-3300.0m, 0.0m, 80.0m)
+    aircraft_rot = RotZYX(roll=0.00, pitch=0.00, yaw=0.00)
 
     # Camera configurations
     cam_config_centered = CAMERA_CONFIG_CENTERED
@@ -31,7 +31,7 @@ PrecompileTools.@compile_workload begin
     for cam_config in [cam_config_centered, cam_config_offset]
         # Generate typical projections with noise
         projections = [
-            project(aircraft_pos, aircraft_rot, corner, cam_config) + ProjectionPoint(0.1px, 0.1px)
+            project(aircraft_pos, aircraft_rot, corner, cam_config)
             for corner in runway_corners
         ]
 
