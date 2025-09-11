db33e4f8c6302567733d5790a600b29fd713fe33
Romeo Valentin <romeov@stanford.edu>
2025-09-11 17:07:50 +0200

    Fix up vcat type inference

--- src/pose_estimation/optimization.jl
+++ src/pose_estimation/optimization.jl
@@ -71,16 +71,17 @@ end
 
 "From optimization space into regular space."
 optvar2nominal(x, ps::PoseOptimizationParams3DOF) = [-exp(x[1]); x[2]; exp(x[3])]
-optvar2nominal(x, ps::PoseOptimizationParams6DOF) = [
-    -exp(x[1]); x[2]; exp(x[3]);
-    RotZYX(RodriguesParam(x[4], x[5], x[6])) |> Rotations.params
-]
+# we need `reduce(vcat, ...)` here instead of [... ; ...] for type inference...
+optvar2nominal(x, ps::PoseOptimizationParams6DOF) = reduce(vcat, [
+    [-exp(x[1]); x[2]; exp(x[3])];
+    RotZYX(RodriguesParam(x[4], x[5], x[6])) |> Rotations.params |> Array
+])
 "From regular space into optimization space."
 nominal2optvar(x, ps::PoseOptimizationParams3DOF) = [log(-x[1]); x[2]; log(x[3])]
-nominal2optvar(x, ps::PoseOptimizationParams6DOF) = [
-    log(-x[1]); x[2]; log(x[3]);
-    RodriguesParam(RotZYX(x[4], x[5], x[6])) |> Rotations.params
-]
+nominal2optvar(x, ps::PoseOptimizationParams6DOF) = reduce(vcat, [
+    [log(-x[1]); x[2]; log(x[3])];
+    RodriguesParam(RotZYX(x[4], x[5], x[6])) |> Rotations.params |> Array
+])
 
 """
     pose_optimization_objective(pose_params, ps)
