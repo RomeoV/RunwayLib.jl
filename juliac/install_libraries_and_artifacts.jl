bad1b86227170db6aa95b5cb7a0a49ff64aa5c51
Romeo Valentin <romeov@stanford.edu>
2025-10-13 15:04:37 -0700

    Delete old juliac artifacts

--- juliac/install_libraries_and_artifacts.jl
+++
@@ -1,8 +0,0 @@
-using PackageCompiler
-ctx = PackageCompiler.create_pkg_context(".")
-PackageCompiler.bundle_artifacts(ctx, ENV["COMPILED_DIR"]; include_lazy_artifacts=false)
-stdlibs = PackageCompiler.gather_stdlibs_project(ctx)
-PackageCompiler.bundle_julia_libraries(ENV["COMPILED_DIR"], stdlibs)
-for (root, _, files) in walkdir(ENV["COMPILED_DIR"]), file in files
-  (contains(file, "libjulia-codegen") || contains(file, "libLLVM")) && rm(joinpath(root, file); force=true)
-end
