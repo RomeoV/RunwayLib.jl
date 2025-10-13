bd42a56c3faa7beb4799f4593cc595d544a3d7b7
Romeo Valentin <romeov@stanford.edu>
2025-10-12 23:04:37 -0700

    Make sure the `optvar2nominal` returns original array type
    we had this always return sarray, which leads to downstream issues...

--- src/pose_estimation/optimization.jl
+++ src/pose_estimation/optimization.jl
@@ -32,21 +32,21 @@ struct PoseOptimizationParams3DOF{
 end
 
 "From optimization space into regular space."
-optvar2nominal(x, ps::PoseOptimizationParams3DOF) = SA[-exp(x[1]); x[2]; exp(x[3])]
-optvar2nominal(x, ps::PoseOptimizationParams6DOF) = reduce(
+optvar2nominal(x::AT, ps::PoseOptimizationParams3DOF) where {AT} = SA[-exp(x[1]); x[2]; exp(x[3])] |> AT
+optvar2nominal(x::AT, ps::PoseOptimizationParams6DOF) where {AT} = reduce(
     vcat, (
         SA[-exp(x[1]); x[2]; exp(x[3])],
         RotZYX(RodriguesParam(x[4], x[5], x[6])) |> Rotations.params,
     )
-)
+) |> AT
 "From regular space into optimization space."
-nominal2optvar(x, ps::PoseOptimizationParams3DOF) = SA[log(-x[1]); x[2]; log(x[3])]
-nominal2optvar(x, ps::PoseOptimizationParams6DOF) = reduce(
+nominal2optvar(x::AT, ps::PoseOptimizationParams3DOF) where {AT} = SA[log(-x[1]); x[2]; log(x[3])] |> AT
+nominal2optvar(x::AT, ps::PoseOptimizationParams6DOF) where {AT} = reduce(
     vcat, (
         SA[log(-x[1]); x[2]; log(x[3])],
         RodriguesParam(RotZYX(x[4], x[5], x[6])) |> Rotations.params,
     )
-)
+) |> AT
 
 """
     pose_optimization_objective(pose_params, ps)
