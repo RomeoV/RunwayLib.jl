e88f6ee46a2a145b97012525afdc34f901fece59
Romeo Valentin <romeov@stanford.edu>
2025-10-13 15:18:03 -0700

    Add juliac-script that actually runs juliac

---
+++ juliac/juliac-script.jl
@@ -0,0 +1,26 @@
+using JuliaC
+builddir = isempty(ARGS) ? "build" : ARGS[1]
+
+img = ImageRecipe(
+    output_type="--output-lib",
+    file="./loadrunwaylib.jl",
+    project="..",
+    enable_trim=true,
+    trim_mode="safe",
+    add_ccallables=true,
+    verbose=true
+)
+link = LinkRecipe(
+    image_recipe=img,
+    outname=joinpath(builddir, "lib"),
+    rpath=nothing
+)
+
+bun = BundleRecipe(
+    link_recipe=link,
+    output_dir=builddir
+)
+
+compile_products(img)
+link_products(link)
+bundle_products(bun)
