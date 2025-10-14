494bad544faf29288857fa3546cf4a665e155f03
Romeo Valentin <romeov@stanford.edu>
2025-10-13 21:31:22 -0700

    Add getting_started section

--- docs/make.jl
+++ docs/make.jl
@@ -5,7 +5,15 @@ makedocs(
     sitename="RunwayLib",
     format=Documenter.HTML(),
     modules=[RunwayLib],
-    repo=Remotes.GitHub("RomeoV", "RunwayLib.jl")
+    repo=Remotes.GitHub("RomeoV", "RunwayLib.jl"),
+    pages=[
+        "RunwayLib.jl: Fast Pose Estimation and Runtime Assurance for Runway Landings." => "index.md",
+        "Getting started" => "getting_started.md",
+        # Camera Models
+        # Noise Models
+        # C Interface
+        # Julia interface
+    ]
 )
 
 # Documenter can also automatically deploy documentation to gh-pages.
