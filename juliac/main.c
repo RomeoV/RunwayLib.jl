09829ff01dabf3e898c2b6a75c7f9721055db8ba
Romeo Valentin <romeov@stanford.edu>
2025-10-17 17:52:16 -0700

    Update `main.c` and `libposeest.h`
    `libposeest.h` is actually missing quite a bit still, but we'll start
    with this.

--- juliac/main.c
+++ juliac/main.c
@@ -1,35 +1,50 @@
-/* #include <stdio.h> */
-/* #include "libposeest.h" */
-
-/* int main() { */
-/*     printf("Calling test_estimators()...\n"); */
-/*     int result; */
-/*     for (int i = 0; i < 1; i++) */
-/*         result = test_estimators(); */
-/*     printf("Result: %d\n", result); */
-/*     return 0; */
-/* } */
-
+#include "libposeest.h"
 #include <stdio.h>
 #include <time.h>
-#include "libposeest.h"
 
 int main() {
-    clock_t start, end;
+    WorldPointF64 world_points[] = {
+        {0, 50, 0},
+        {3000, 50, 0},
+        {3000, -50, 0},
+        {0, -50, 0},
+    };
+    ProjectionPointF64 projections[] = {
+        {1908.3869731285338, 1413.7171688399328},
+        {1982.0042057862263, 1086.0904145429126},
+        {2127.0552446444053, 1082.2921194158434},
+        {2269.5962158465413, 1404.2585719400536},
+    };
+    int num_points = 4;
 
-    // First call (includes initialization)
-    start = clock();
-    test_estimators();
-    end = clock();
-    printf("First call: %.6f seconds\n", ((double)(end - start)) / CLOCKS_PER_SEC);
+    PoseEstimateF64 pose_result;
+    CameraMatrixF64 cammat = {
+        // camera matrix row-by-row, will be transposed when loading into Julia,
+        // which is column-major.
+        {-7246.4, 0.0, 2048.0, 0.0, -7246.4, 1500.0, 0.0, 0.0, 1.0},
+        4096.0,
+        3000.0};
+    WorldPointF64 initial_guess_pos = {-1000, 0, 150};
+    RotationF64 initial_guess_rot = {0., 0., 0.};
 
-    // Subsequent calls
-    for(int i = 0; i < 5; i++) {
-        start = clock();
-        test_estimators();
-        end = clock();
-        printf("Call %d: %.6f seconds\n", i+2, ((double)(end - start)) / CLOCKS_PER_SEC);
+    int result = estimate_pose_6dof(world_points, projections, num_points, NULL,
+                                    COV_DEFAULT, &cammat, &initial_guess_pos,
+                                    &initial_guess_rot, &pose_result);
+
+    if (result != POSEEST_SUCCESS) {
+        printf("Error: %s\n", get_error_message(result));
+        printf("Details: %s\n", get_last_error_detail());
+        return 1;
     }
+
+    printf("Pose estimated successfully\n");
+    printf("Position: (%.2f, %.2f, %.2f) m\n", pose_result.position.x,
+           pose_result.position.y, pose_result.position.z);
+    printf("Rotation (yaw, pitch, roll): (%.4f, %.4f, %.4f) rad\n",
+           pose_result.rotation.yaw, pose_result.rotation.pitch,
+           pose_result.rotation.roll);
+    printf("Residual norm: %.6f\n", pose_result.residual_norm);
+    printf("Converged: %s\n", pose_result.converged ? "yes" : "no");
 
     return 0;
 }
